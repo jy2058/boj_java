c96e8d4d100921dce32c49388904f83ca2daeb52
KJY <jiy3285@gmail.com>
2022-01-04 11:55:46 +0900

    기초1 - 203_1935 후위표기식2

--- src/basic1/step203/boj_1935.java
+++ src/basic1/step203/boj_1935.java
@@ -12,24 +12,42 @@ public class boj_1935 {
         int N = Integer.parseInt(br.readLine());
         // 입력 수 배열
         int[] num = new int[N];
-        // 문자 배열?
-        int[] chr = new int[27];
-        // 결과 스택?
-        Stack<String> chrStack = new Stack<>();
-        // 연산자스택
-        Stack<String> oprStack = new Stack<>();
+        // 스택
+        Stack<Double> stack = new Stack<>();
 
-        // 입력 문자, 연산자
         String s = br.readLine();
-        // 입력 수
+
         for (int i = 0; i < N; i++) {
             num[i] = Integer.parseInt(br.readLine());
         }
 
-        // s 돌면서 문자는 해당 알파벳에 맞는 값 찾아와 결과스택에, 연산은 연산스택에 추가하면서
-        // 연산자를 만났을 때 문자스택의 길이가 2면 연산 수행 후 결과를 스택에 다시 추가
-        // 그 외엔 문자, 연산 스택에 계속 추가
+        for (int i = 0; i < s.length(); i++) {
+            char target = s.charAt(i);
+            if ('A' <= target && target <= 'Z') {
+                double d = num[target - 'A'];
+                stack.push(d);
+            } else {
+                double num1 = stack.pop();
+                double num2 = stack.pop();
+                double result = 0.0;
 
-
+                switch (target) {
+                    case '+':
+                        result = num2 + num1;
+                        break;
+                    case '-':
+                        result = num2 - num1;
+                        break;
+                    case '*':
+                        result = num2 * num1;
+                        break;
+                    case '/':
+                        result = num2 / num1;
+                        break;
+                }
+                stack.push(result);
+            }
+        }
+        System.out.printf("%.2f", stack.pop());
     }
 }
